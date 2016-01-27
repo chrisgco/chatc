d2b789395ef384eb32e5c7f8e5293c779a4dcbfd
Chris Grant <chris@chrisgrant.co>
2016-01-26 16:02:49 -0800

    added makefile

--- client.c
+++ client.c
@@ -8,7 +8,58 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+
+/*
+clang client.c 104.129.86.9314 - Connect to specific server
+
+login or signup: login
+
+username: iieqwuigfeiwq
+password: hwqihiqhii
+
+connected
+
+failed to connect
+
+conversations:
+- @john john smith
+- @kyle kyle roberts
+
+who do you want to talk to (@username): @kyle
+
+Message History with Kyle Roberts (@kyle):
+
+Kyle (Dec, 5, 2015 at 10:01pm): hewifh ewhfoewqhfewh
+You (Dec 5, 2015 at 10:02pm): hhefhew ehwifeho heiwoq
+
+Compose: hey what's up
+
+You (Dec 6, 2015 at 4:59pm): hey what's up
+
+Compose:
+
+Kyle (Dec 6, 2015 at 5:00pm): Hey
+
+Compose:
+
+
+
+
+
+ */
+
 int main(int argc, char **argv) {
+
+
+  if( argc == 2 ) {
+     printf("The argument supplied is %s\n", argv[1]);
+  }
+  else if( argc > 2 ) {
+     printf("Too many arguments supplied.\n");
+  }
+  else {
+     printf("One argument expected.\n");
+  }
 
   int socket_id;
   char buffer[256];
