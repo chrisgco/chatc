581ccc5783cb5dcebd8091e6181e4368bb7f2b0e
Chris Grant <chris@chrisgrant.co>
2016-01-26 20:46:45 -0800

    um

--- client.c
+++ client.c
@@ -9,32 +9,27 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
-
-// int main(int argc, char **argv) {
-//   char server_addr[16];
-//
-//   if( argc == 2 ) {
-//     strcpy(server_addr, argv[1]);
-//   } else {
-//     printf("Please supply chat server address: \"./client 104.159.68.32\"\n");
-//     return 0;
-//   }
+/* Checks is a string starts with another */
+bool starts_with(const char *pre, const char *str){
+    return strncmp(pre, str, strlen(pre)) == 0;
+}
 
 int main(int argc, char *argv[]) {
-   int sockfd, portno, n;
+   int sockfd, port, n;
    struct sockaddr_in serv_addr;
    struct hostent *server;
 
    char buffer[256];
+   char usrname_buffer[30];
+   char message_buffer[232];
 
+   /* START set up connection to server */
    if (argc < 3) {
-      fprintf(stderr,"usage %s hostname port\n", argv[0]);
+      fprintf(stderr,"usage %s host port\n", argv[0]);
       exit(0);
    }
 
-   portno = atoi(argv[2]);
-
-   /* Create a socket point */
+   port = atoi(argv[2]);
    sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
    if (sockfd < 0) {
@@ -52,39 +47,87 @@ int main(int argc, char *argv[]) {
    bzero((char *) &serv_addr, sizeof(serv_addr));
    serv_addr.sin_family = AF_INET;
    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-   serv_addr.sin_port = htons(portno);
+   serv_addr.sin_port = htons(port);
 
-   /* Now connect to the server */
    if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
       perror("ERROR connecting");
       exit(1);
    }
+   /* END set up connection to server */
 
-   /* Now ask for a message from the user, this message
-      * will be read by server
-   */
+   /* START choose username */
+   printf("Choose a username: ");
+   bzero(usrname_buffer, 31); //clear username_buffer
+   fgets(usrname_buffer, 30, stdin); //read from stdin
+   bzero(buffer,256); // clear buffer
+   strcpy(buffer, "/SIGNUP/"); // copy /SIGNUP/ into buffer
+   strcat(buffer, usrname_buffer); //concatentate buffer and username to get /SIGNUP/username
 
-   printf("Please enter the message: ");
-   bzero(buffer,256);
-   fgets(buffer,255,stdin);
-
-   /* Send message to the server */
    n = write(sockfd, buffer, strlen(buffer));
 
-   if (n < 0) {
-      perror("ERROR writing to socket");
-      exit(1);
-   }
+  if (n < 0) {
+    perror("ERROR writing to socket");
+    exit(1);
+  }
 
-   /* Now read server response */
-   bzero(buffer,256);
-   n = read(sockfd, buffer, 255);
+  // Get response from server
+  bzero(buffer, 256);
+  n = read(sockfd, buffer, 255);
 
-   if (n < 0) {
-      perror("ERROR reading from socket");
-      exit(1);
-   }
+  if (n < 0) {
+    perror("ERROR reading from socket");
+    exit(1);
+  }
 
-   printf("%s\n",buffer);
-   return 0;
+
+  if(strcmp("/SUCCESS", buffer) == 0) { //if successful
+    while(1) {
+      // Send Message
+      printf("Compose: ");
+      bzero(buffer,256);
+      bzero(message_buffer,232);
+      fgets(message_buffer,255,stdin);
+
+      strcpy(buffer, "/MESSAGE/");
+      strcat(buffer, message_buffer);
+
+      n = write(sockfd, buffer, strlen(buffer));
+
+      if (n < 0) {
+         perror("ERROR writing to socket");
+         exit(1);
+      }
+
+      /* Check for response */
+      bzero(buffer,256);
+      n = read(sockfd, buffer, 255);
+
+      if (n < 0) {
+         perror("ERROR reading from socket");
+         exit(1);
+      }
+
+      // Read from server for new messages
+      while(1) {
+        bzero(buffer,256);
+        n = read(sockfd, buffer, 255);
+
+        if (n < 0) {
+           perror("ERROR reading from socket");
+           exit(1);
+        }
+
+        if(starts_with("/MESSAGE/", buffer)) {
+          // TODO: parse message /MESSAGE/username/message...
+        }
+      }
+      printf("%s\n",buffer);
+    }
+  } else {
+    printf("Username was taken or the room is full, try again later...\n");
+  }
+  /* END choose username */
+
+
+  return 0;
 }
--- server.c
+++ server.c
@@ -2,40 +2,82 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-void doprocessing (int sock) {
-   int n;
-   char buffer[256];
-   bzero(buffer,256);
-   n = read(sock,buffer,255);
+#define MAX_CLIENTS	128
+/* Client structure */
+typedef struct {
+	struct sockaddr_in addr;	/* Client remote address */
+	int fd;			/* Connection file descriptor */
+	char usrname[32];			/* Client name */
+} client_t;
 
-   if (n < 0) {
-      perror("ERROR reading from socket");
-      exit(1);
-   }
+client_t *clients[MAX_CLIENTS];
 
-   printf("Here is the message: %s\n",buffer);
-   n = write(sock,"I got your message",18);
-
-   if (n < 0) {
-      perror("ERROR writing to socket");
-      exit(1);
-   }
-
+bool add_client(client_t *client){
+	int i;
+	for(i=0;i<MAX_CLIENTS;i++){
+    if(strcmp(clients[i]->usrname, client->usrname)) {
+      return false;
+    }
+		if(!clients[i]){
+			clients[i] = client;
+			return true;
+		}
+	}
+  return false;
 }
 
-int main( int argc, char *argv[] ) {
-   int sockfd, newsockfd, portno;
-   socklen_t clilen;
-   char buffer[256];
-   struct sockaddr_in serv_addr, cli_addr;
-   int n, pid;
+void remove_client(char usrname[32]){
+	int i;
+	for(i=0;i<MAX_CLIENTS;i++){
+		if(clients[i]){
+			if(strcmp(clients[i]->usrname, usrname) == 0){
+				clients[i] = NULL;
+				return;
+			}
+		}
+	}
+}
 
-   /* First call to socket() function */
+void send_message(char *message, char usrname[32]){
+	int i;
+  char buffer[255];
+
+	for(i=0;i<MAX_CLIENTS;i++){
+		if(clients[i]){
+			if(strcmp(clients[i]->usrname, usrname) != 0){
+        // concatentate strings to get /MESSAGE/username/message
+        bzero(buffer, 256);
+        strcat(buffer, clients[i]->usrname);
+        strcat(buffer, "/");
+        strcat(buffer, message);
+
+
+				write(clients[i]->fd, message, strlen(message));
+			} else {
+        write(clients[i]->fd, "/SUCCESS", 8);
+      }
+		}
+	}
+}
+
+bool starts_with(const char *pre, const char *str){
+    return strncmp(pre, str, strlen(pre)) == 0;
+}
+
+void handle_client (int client_fd, struct sockaddr_in client_addr);
+
+int main( int argc, char *argv[] ) {
+   int sockfd, client_fd;
+   socklen_t client_len;
+   struct sockaddr_in server_addr, client_addr;
+   int pid;
+
    sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
    if (sockfd < 0) {
@@ -43,32 +85,25 @@ int main( int argc, char *argv[] ) {
       exit(1);
    }
 
-   /* Initialize socket structure */
-   bzero((char *) &serv_addr, sizeof(serv_addr));
-   portno = 5001;
+   bzero((char *) &server_addr, sizeof(server_addr));
 
-   serv_addr.sin_family = AF_INET;
-   serv_addr.sin_addr.s_addr = INADDR_ANY;
-   serv_addr.sin_port = htons(portno);
+   server_addr.sin_family = AF_INET;
+   server_addr.sin_addr.s_addr = INADDR_ANY;
+   server_addr.sin_port = htons(5000);
 
-   /* Now bind the host address using bind() call.*/
-   if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+   if (bind(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
       perror("ERROR on binding");
       exit(1);
    }
 
-   /* Now start listening for the clients, here
-      * process will go in sleep mode and will wait
-      * for the incoming connection
-   */
 
    listen(sockfd, 5);
-   clilen = sizeof(cli_addr);
+   client_len = sizeof(client_addr);
 
    while (1) {
-      newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+      client_fd = accept(sockfd, (struct sockaddr *) &client_addr, &client_len);
 
-      if (newsockfd < 0) {
+      if (client_fd < 0) {
          perror("ERROR on accept");
          exit(1);
       }
@@ -84,12 +119,66 @@ int main( int argc, char *argv[] ) {
       if (pid == 0) {
          /* This is the client process */
          close(sockfd);
-         doprocessing(newsockfd);
+         handle_client(client_fd, client_addr);
          exit(0);
       }
       else {
-         close(newsockfd);
+         close(client_fd);
       }
 
+		sleep(1);
+
    } /* end of while */
+
+}
+
+void handle_client (int client_fd, struct sockaddr_in client_addr) {
+   int n;
+   char buffer[256];
+   bzero(buffer,256);
+   n = read(client_fd, buffer, 255);
+
+   if (n < 0) {
+      perror("ERROR reading from socket");
+      exit(1);
+   }
+
+   if (starts_with("/SIGNUP/", buffer)) {
+      client_t *client = (client_t *)malloc(sizeof(client_t));
+      client->addr = client_addr;
+      client->fd = client_fd;
+      strcpy(client->usrname, strtok(buffer, "/SIGNUP/"));
+      if (add_client(client)) {
+        write(client_fd, "/SUCCESS", 8);
+      } else {
+        write(client_fd, "/FAILURE", 8);
+      }
+   } else if (starts_with("/MESSAGE/", buffer)) {
+     char *message = strtok(buffer, "/MESSAGE/");
+     int i;
+     for (i = 0; i < MAX_CLIENTS; i++) {
+       if (clients[i]) {
+         if (clients[i]->fd == client_fd) {
+          send_message(message, clients[i]->usrname);
+         }
+       }
+     }
+   } else if (starts_with("/BYE/", buffer)) {
+     int i;
+     for (i = 0; i < MAX_CLIENTS; i++) {
+       if (clients[i]) {
+         if (clients[i]->fd == client_fd) {
+           clients[i] = NULL;
+         }
+       }
+     }
+   } else {
+     n = write(client_fd, "/WTF", 4);
+   }
+
+   if (n < 0) {
+      perror("ERROR writing to socket");
+      exit(1);
+   }
+
 }
